cecd62f9178799785494ca1486c17e7f642ed010
iamcauringap <gcaurin@gmail.com>
2021-03-20 22:28:04 -0300

    pequenas mudanças formatação

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,8 @@
 int main (){
 
     // prints hello world 
-    std::cout << "Learning to work with version control \n" << "Installed Git on Windows \n" ;
+    std::cout << "\n" << "Learning to work with version control \n" ;
+    std::cout << "\n" << "Installed Git on Windows \n" ;
     std::cout << "\n" << "Create Project on Github learning \n" ;
     std::cout << "\n" << "Para executar digite ponto barra invertida hello.exe \n" ;
     return 0;
