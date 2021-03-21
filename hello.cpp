5758a67f8911837c0cfb70e59d8aef7ceb189ae0
iamcauringap <gcaurin@gmail.com>
2021-03-20 21:59:20 -0300

    inseri comentarios

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,7 @@ int main (){
 
     // prints hello world 
     std::cout << "Learning to work with version control \n" << "Installed Git on Windows \n" ;
+    std::cout << "\n" << "Create Project on Github learning \n" ;
+    std::cout << "\n" << "Para executar digite ponto barra invertida hello.exe \n" ;
     return 0;
-}
+} 
